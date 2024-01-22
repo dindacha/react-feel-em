1a91c98035129c35dfe4e045bff2a177b00e6e6a
dindacha <dindacha3@gmail.com>
2024-01-22 16:46:32 +0700

    first commit initialize project feel-em

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const apiKey = process.env.REACT_APP_APIKEY
+const baseUrl = process.env.REACT_APP_BASEURL
+
+
+export const getMovieList = async() => {
+    try {
+        const movie = await axios.get(`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`)
+        return movie.data.results;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+
+}
+
+export const searchMovie = async(q) => {
+    const search = await axios.get(`${baseUrl}/search/movie?query=${q}page=1&api_key=${apiKey}`)
+    return search.data
+}
