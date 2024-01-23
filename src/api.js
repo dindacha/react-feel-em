e84ea4e9d611efd42778d426ab99f9e8a2082fd1
dindacha <dindacha3@gmail.com>
2024-01-23 13:27:08 +0700

    fix typo in the URL string

--- src/api.js
+++ src/api.js
@@ -15,6 +15,11 @@ export const getMovieList = async() => {
 }
 
 export const searchMovie = async(q) => {
-    const search = await axios.get(`${baseUrl}/search/movie?query=${q}page=1&api_key=${apiKey}`)
-    return search.data
+    try {
+        const search = await axios.get(`${baseUrl}/search/movie?query=${q}&page=1&api_key=${apiKey}`)
+        return search.data
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
